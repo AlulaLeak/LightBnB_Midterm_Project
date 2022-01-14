368dd4dc3acdd30d68682a6faa9f11e20abe99af
fathel mohamud <fathel.mohamud@gmail.com>
2022-01-14 04:31:24 -0500

    cookies

--- routes/register.js
+++ routes/register.js
@@ -8,6 +8,7 @@
 const express = require('express');
 const router  = express.Router();
 const app = express()
+const cookieSession = require('cookie-session')
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -28,6 +29,40 @@ module.exports = (db) => {
     // This is where we need to set a cookie
 
   })
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    const email = req.body.email;
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const values = [email];
+    const userQuery = `
+    SELECT email
+    FROM users
+    WHERE email = $1
+    `
+    db.query(userQuery, values)
+    .then((result) => {
+      console.log(result)
+      if (email === '' && hashedPassword === '') {
+        return res.status(400).send('Email & password cannot be empty')
+      }
+      const userFound = getUserByEmail(email, result);
+      if (userFound) {
+        res.status(400).send('Sorry, that user already exists!');
+        return;
+      }
+      const userID = createUser(email, hashedPassword, result)
+      req.session.user_id = userID
+      res.redirect('/')
+    })
+
+  })
+
+
+
   return router;
 };
 
+app.use(cookieSession({
+    name: 'session',
+    keys: [/* secret keys */],
+}))
